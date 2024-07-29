fb87d15051fc31ca3d5d807bc60eeaa96205ff64
Midival <lukehays32@gmail.com>
2024-07-28 20:47:32 -0400

    Add Astro DB to project

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,21 +1,19 @@
 import { defineConfig } from 'astro/config';
 import { viteStaticCopy } from "vite-plugin-static-copy";
+import db from "@astrojs/db";
 
+// https://astro.build/config
 export default defineConfig({
   vite: {
-    plugins: [
-      viteStaticCopy({
-        targets: [
-          {
-            src: "node_modules/@shoelace-style/shoelace/dist/assets/icons",
-            dest: "assets",
-          },
-          {
-            src: 'node_modules/@shoelace-style/shoelace/dist/themes',
-            dest: ""
-          }
-        ],
-      }),
-    ],
+    plugins: [viteStaticCopy({
+      targets: [{
+        src: "node_modules/@shoelace-style/shoelace/dist/assets/icons",
+        dest: "assets"
+      }, {
+        src: 'node_modules/@shoelace-style/shoelace/dist/themes',
+        dest: ""
+      }]
+    })]
   },
+  integrations: [db()]
 });
