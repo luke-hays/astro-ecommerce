28f789d559e8799841d5049a0574c59b929cdfb8
Midival <lukehays32@gmail.com>
2024-08-10 09:43:03 -0400

    Add SSR for form handling and modify registration form

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,9 @@
 import { defineConfig } from 'astro/config';
 import { viteStaticCopy } from "vite-plugin-static-copy";
-
 import db from "@astrojs/db";
 import sentry from "@sentry/astro";
+
+import netlify from "@astrojs/netlify";
 
 // https://astro.build/config
 export default defineConfig({
@@ -17,14 +18,13 @@ export default defineConfig({
       }]
     })]
   },
-  integrations: [
-    db(), 
-    sentry({
-      dsn: "https://bfe09e41e9c48044a719c19f773646ee@o4507714165211136.ingest.us.sentry.io/4507714193719296",
-      sourceMapsUploadOptions: {
-        project: "astro-ecommerce",
-        authToken: process.env.SENTRY_AUTH_TOKEN,
-      },
-    })
-  ]
+  integrations: [db(), sentry({
+    dsn: "https://bfe09e41e9c48044a719c19f773646ee@o4507714165211136.ingest.us.sentry.io/4507714193719296",
+    sourceMapsUploadOptions: {
+      project: "astro-ecommerce",
+      authToken: process.env.SENTRY_AUTH_TOKEN
+    }
+  })],
+  output: "server",
+  adapter: netlify()
 });
