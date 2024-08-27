580193bce48fba040dda894c025e7f03bb57d524
Midival <lukehays32@gmail.com>
2024-08-26 20:51:33 -0400

    Add cart table for persistent storage. Assign user a cookie when arriving to page

--- db/config.ts
+++ db/config.ts
@@ -1,6 +1,17 @@
-import { defineDb } from "astro:db";
+import { defineDb, defineTable, column } from "astro:db";
+
+const Cart = defineTable({
+  columns: {
+    sessionId: column.text({ primaryKey: true }),
+    items: column.json(),
+    lastUpdatedDate: column.date()
+  }
+})
 
 // https://astro.build/db/config
 export default defineDb({
-  tables: {},
+  tables: {
+    Cart
+  },
 });
+ 
