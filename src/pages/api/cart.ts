9b309425f56f30ddf6a268203d1d26f500725e77
Midival <lukehays32@gmail.com>
2024-08-26 22:14:33 -0400

    Add GET endpoint for cart

--- src/pages/api/cart.ts
+++ src/pages/api/cart.ts
@@ -1,5 +1,17 @@
 import type { APIRoute } from 'astro'
-import { db, Cart as CartTable, like } from 'astro:db'
+import { db, Cart as CartTable, eq } from 'astro:db'
+
+export const GET: APIRoute = async ({ request }) => {
+  const sessionId = request.headers.get('cookie')?.split('=')[1] ?? ''
+  
+  const record = await db.select().from(CartTable).where(
+    eq(CartTable.sessionId, sessionId)
+  )
+
+  return new Response(
+    JSON.stringify(record),
+  )
+}
 
 export const POST: APIRoute = async ({ request }) => {
   const sessionId = request.headers.get('cookie')?.split('=')[1] ?? ''
@@ -10,7 +22,7 @@ export const POST: APIRoute = async ({ request }) => {
 
   // Check if user has existing session
   const record = await db.select().from(CartTable).where(
-    like(CartTable.sessionId, sessionId)
+    eq(CartTable.sessionId, sessionId)
   )
 
   if (record.length > 0) {
