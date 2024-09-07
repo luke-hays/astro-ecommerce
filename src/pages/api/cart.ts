0a5183fe414ce09c14e065b81c27734b07f0eda8
Luke Hays <lukehays32@gmail.com>
2024-09-07 00:43:41 -0400

    add endpoint to remove items

--- src/pages/api/cart.ts
+++ src/pages/api/cart.ts
@@ -42,3 +42,57 @@ export const POST: APIRoute = async ({ cookies, request }) => {
     },
   });
 };
+
+export const DELETE: APIRoute = async ({ cookies, request }) => {
+  let sessionId = cookies.get('cart')?.value
+
+  if (!sessionId) {
+    sessionId = uuid()
+  }
+
+  const url = new URL(request.url)
+  const params = new URLSearchParams(url.search)
+
+  const productId = params.get('id')
+
+  if (productId == null) {
+    return new Response(null, {
+      status: 400
+    })
+  }
+
+  // Check if user has existing session
+  const recordResponse = await turso.execute({
+    sql: 'SELECT * FROM cart WHERE session_id = ?;',
+    args: [sessionId]
+  })
+
+  if (recordResponse.rows.length === 0) {
+    return new Response(null, {
+      status: 400
+    })  
+  } else {
+    let items = JSON.parse(recordResponse.rows[0].items as any) as Cart
+    
+    if (productId in items) {
+      items = Object.fromEntries(Object.entries(items).filter(([k, _]) => k !== productId))
+
+      await turso.execute({
+        sql: 'UPDATE cart SET items = ? WHERE session_id = ?;',
+        args: [JSON.stringify(items), sessionId]
+      })
+
+      return new Response(JSON.stringify(items), {
+        status: 200,
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+    } else {
+      return new Response(null, {
+        status: 400
+      })
+    }
+  }
+};
+
