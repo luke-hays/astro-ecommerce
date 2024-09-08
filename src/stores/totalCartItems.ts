84415c8262b75a50a2c06afcf8482f0836f170df
Luke Hays <lukehays32@gmail.com>
2024-09-08 17:57:53 -0400

    Switch cart state handling to store

--- src/stores/totalCartItems.ts
+++ src/stores/totalCartItems.ts
@@ -1,3 +1,8 @@
-import { atom } from "nanostores";
+import { computed } from "nanostores";
+import { $cart } from "./cart";
 
-export const $totalCartItems = atom(0);
+// Computed store will update its value whenever this dependency store $cart is updated
+export const $totalCartItems = computed($cart, cart => {
+  return Object.values(cart).reduce((prev, curr) => prev + curr, 0)
+});
+ 
